15606148393e464bcd4e603f73899bd38efab960
Daniil Ruzin <d.ruzin@adaurum.ru>
2023-05-23 13:59:58 +0300

    Move RowsImportController to separate folder

--- app/Http/Controllers/Rows/ImportController.php
+++ app/Http/Controllers/Rows/ImportController.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Rows;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\Rows\ImportExcelRequest;
 
-class RowsImportController extends Controller
+class ImportController extends Controller
 {
     public function uploadExcel(ImportExcelRequest $request)
     {
