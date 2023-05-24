8d47045a423510e65b35339bd469760282f7a141
Daniil Ruzin <d.ruzin@adaurum.ru>
2023-05-24 22:24:38 +0300

    fix Row model name

--- app/Models/Row.php
+++ app/Models/Row.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Rows extends Model
+class Row extends Model
 {
     use HasFactory;
 }
