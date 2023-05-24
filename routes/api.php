0f17dc7d3231f37e574c401558c19a5a82b0d2d8
Daniil Ruzin <d.ruzin@adaurum.ru>
2023-05-24 16:31:49 +0300

    add auth.basic middleware for rows.import.excel

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\Rows\ImportController as RowsImportController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,8 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::post('/rows/import/excel', [RowsImportController::class, 'importExcel'])->name('rows.import.excel');
+Route::middleware('auth.basic')
+    ->post('/rows/import/excel', [RowsImportController::class, 'importExcel'])
+    ->name('rows.import.excel');
