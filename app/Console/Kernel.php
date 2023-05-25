ec461a26e75b7672be1e22a49d38d6ef46e1ba94
Daniil Ruzin <d.ruzin@adaurum.ru>
2023-05-25 09:02:11 +0300

    add rows_import_queue worker in ConsoleKernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('php artisan queue:work --queue=rows_import_queue --stop-when-empty')->cron('* * * * *')->runInBackground();
     }
 
     /**
